4f6ad59ea9284ea39a7905ad5b0b3902bb14b8e0
shakir raza <shakirraza737@gmail.com>
2024-11-15 21:06:10 +0300

    initial

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -51,12 +51,12 @@ const MeetingRoom = () => {
 
   return (
     <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
-      <div className="relative flex size-full items-center justify-center">
-        <div className=" flex size-full max-w-[1000px] items-center">
+      <div className="relative flex size-full items-center justify-center overflow-x-auto">
+        <div className=" flex size-full max-w-[1000px]">
           <CallLayout />
         </div>
         <div
-          className={cn('h-[calc(100vh-86px)] hidden ml-2', {
+          className={cn('h-[calc(90vh-80px)] w-1/6 hidden ml-2', {
             'show-block': showParticipants,
           })}
         >
@@ -64,11 +64,11 @@ const MeetingRoom = () => {
         </div>
       </div>
       {/* video layout and call controls */}
-      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
-        <CallControls onLeave={() => router.push(`/`)} />
+      <div className="fixed bottom-0 flex w-full md:items-center justify-center gap-1 overflow-x-auto">
+        <CallControls onLeave={() => router.push(`/`)}/>
 
-        <DropdownMenu>
-          <div className="flex items-center">
+        {/* <DropdownMenu>
+          <div className="flex items-center ">
             <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
               <LayoutList size={20} className="text-white" />
             </DropdownMenuTrigger>
@@ -87,8 +87,7 @@ const MeetingRoom = () => {
               </div>
             ))}
           </DropdownMenuContent>
-        </DropdownMenu>
-        <CallStatsButton />
+        </DropdownMenu>  */}
         <button onClick={() => setShowParticipants((prev) => !prev)}>
           <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
             <Users size={20} className="text-white" />
@@ -96,6 +95,8 @@ const MeetingRoom = () => {
         </button>
         {!isPersonalRoom && <EndCallButton />}
       </div>
+        <CallStatsButton />
+       
     </section>
   );
 };
